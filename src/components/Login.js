f54a42b90935acc426a10bc906c13d0ee2b5a875
fatemah-alm <fatemah.alm@hotmail.com>
2022-04-17 02:54:01 +0300

    log-in and register pages plus authstore

---
+++ src/components/Login.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import authStore from "../stores/authStore";
+import { observer } from "mobx-react";
+
+const Login = () => {
+  const [user, setUser] = useState();
+  const [isCorrect, setIsCorrect] = useState(true);
+  const navigate = useNavigate();
+  const handleChange = (event) =>
+    setUser({ ...user, [event.target.name]: event.target.value });
+  const handleSubmit = (event) => {
+    setIsCorrect(true);
+    event.preventDefault();
+    authStore.signin(user, navigate, setIsCorrect);
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <h3 className="fonts">Login</h3>
+      <div className="form-group">
+        <label className="label">username</label>
+        <input
+          type="text"
+          name="username"
+          className="form-control label"
+          placeholder="Enter username"
+          onChange={handleChange}
+        />
+      </div>
+      <div className="form-group">
+        <label className="label margin1">Password</label>
+        <input
+          type="password"
+          name="password"
+          className="form-control label"
+          placeholder="Enter password"
+          onChange={handleChange}
+        />
+      </div>
+      {!isCorrect && (
+        <p className="in-correct">* incorrect username or password</p>
+      )}
+      <button type="submit" className="btn .btn-outline-dark btn-block margin1">
+        Submit
+      </button>
+
+      <p className="forgot-password text-right">
+        Forgot <Link to="/">password?</Link>
+      </p>
+    </form>
+  );
+};
+
+export default observer(Login);
---
+++ src/stores/authStore.js
@@ -0,0 +1,72 @@
+import { makeAutoObservable } from "mobx";
+import { instance } from "./instance";
+
+class AuthStore {
+  user = null;
+
+  constructor() {
+    makeAutoObservable(this);
+    // this will turn our class into a mobx store and all components can observe the changes that happen in the store
+  }
+
+  setUser = (token) => {
+    instance.defaults.headers.common.Authorization = `Bearer ${token}`;
+    localStorage.setItem("token2", token);
+    console.log(token);
+  };
+
+  unSetUser = () => {
+    localStorage.removeItem("token2");
+    this.user = null;
+  };
+
+  signup = async (user, navigate, setIsCorrect) => {
+    try {
+      console.log(
+        "🚀 ~ file: authStore.js ~ line 13 ~ AuthStore ~ signup= ~ user",
+        user
+      );
+      await instance.post("/users/", user, { mode: "cors" });
+      await this.signin(user, navigate, setIsCorrect);
+    } catch (error) {
+      setIsCorrect(false);
+
+      console.log(error);
+    }
+  };
+
+  signin = async (user, navigate, setIsCorrect) => {
+    try {
+      const res = await instance.post("/jwt/create/", user);
+      this.setUser(res.data.access);
+      const res2 = await instance.get("/users/me/");
+      console.log(res2.data);
+      this.user = res2.data;
+      await navigate("/home");
+    } catch (error) {
+      setIsCorrect(false);
+      console.log(error);
+    }
+  };
+
+  checkForToken = async () => {
+    const token = localStorage.getItem("token2");
+
+    if (token) {
+      try {
+        // console.log(token);
+        this.setUser(token);
+        const res2 = await instance.get("/users/me/");
+        this.user = res2.data;
+        // console.log(res2.data);
+      } catch (error) {
+        // console.log(error);
+        this.unSetUser();
+      }
+    } else this.unSetUser();
+  };
+}
+
+const authStore = new AuthStore();
+authStore.checkForToken();
+export default authStore;
