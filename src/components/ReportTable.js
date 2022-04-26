653b3c80fb367f20f78facbb78791d22d047526a
fatemah-alm <fatemah.alm@hotmail.com>
2022-04-26 04:28:43 +0300

    evaluation table

--- src/components/ReportTable.js
+++ src/components/ReportTable.js
@@ -1,7 +1,27 @@
 import React from "react";
 import { Table } from "react-bootstrap";
 import { observer } from "mobx-react";
-const ReportTable = () => {
+const ReportTable = ({ project, teamId }) => {
+  console.log(teamId, "TEAM");
+  let length = project.judge.length;
+  let avgScorePerTeam = 0;
+  const handleCriteria = (criteriaId) => {
+    console.log(criteriaId, "ID");
+    project.judge.map((judge) =>
+      judge.grade.map((team) => {
+        if (+team.id === +teamId)
+          team.grade.map((criteria) => {
+            if (+criteria.id === +criteriaId) {
+              console.log("teeest");
+              return (avgScorePerTeam += criteria.grade);
+            }
+          });
+      })
+    );
+
+    console.log(avgScorePerTeam);
+  };
+
   return (
     <Table striped bordered hover style={{ width: "80%", textAlign: "center" }}>
       <thead>
@@ -13,24 +33,19 @@ const ReportTable = () => {
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td>presentaion</td>
-          <td>90%</td>
-          <td>20</td>
-          <td>19</td>
-        </tr>
-        <tr>
-          <td>reading</td>
-          <td>95%</td>
-          <td>30</td>
-          <td>27.5</td>
-        </tr>
-        <tr>
-          <td>Listening</td>
-          <td>86%</td>
-          <td>20</td>
-          <td>16</td>
-        </tr>
+        {project.criterias.map((criteria) => {
+          handleCriteria(criteria.id);
+          return (
+            <tr>
+              <th scope="row">{criteria.name}</th>
+              <td>{(avgScorePerTeam / length) * 10}%</td>
+              <td>{criteria.weight}</td>
+              <td>
+                {((avgScorePerTeam / length) * 0.1 * criteria.weight) / 100}
+              </td>
+            </tr>
+          );
+        })}
       </tbody>
     </Table>
   );
